926d10577bb0733b7c71de1600f0dbb01e443192
scaryCoder <debmalyakuila911@gmail.com>
2026-03-08 00:35:00 +0530

    server: refactor global exception handling

--- server/src/main/java/com/debmalya/urlShortener/CustomExceptions/GlobalExceptionHandler.java
+++ server/src/main/java/com/debmalya/urlShortener/CustomExceptions/GlobalExceptionHandler.java
@@ -1,35 +1,35 @@
 package com.debmalya.urlShortener.CustomExceptions;
 
+import com.debmalya.urlShortener.dtos.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Map<String, Object> handleUsernameExists(UsernameAlreadyExistsException e) {
+    public ErrorResponseDTO handleUsernameExists(UsernameAlreadyExistsException e) {
 
-        return Map.of(
-            "timestamp", LocalDateTime.now(),
-            "status", 409,
-            "error", "Conflict",
-            "message", e.getMessage()
+        return new ErrorResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                "Conflict",
+                e.getMessage()
         );
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, Object> handleGeneralException(Exception e) {
-        return Map.of(
-            "timestamp", LocalDateTime.now(),
-            "status", 500,
-            "error", "Internal Server Error",
-            "message", "Something went wrong"
+    public ErrorResponseDTO handleGeneralException(Exception e) {
+
+        return new ErrorResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                "Something went wrong"
         );
     }
-
 }
